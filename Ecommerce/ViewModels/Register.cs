8938c7473416a1d21d76a214f58d743af39f245b
Anubhav-Verma <ianubhavverma@gmail.com>
2023-03-14 23:30:12 +0530

    Login and register pages created

---
+++ Ecommerce/ViewModels/Register.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Password field is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "The Confirm Password field is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and Confirm Password fields do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "The First Name field is required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The Last Name field is required.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The City field is required.")]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "The Phone Number field is required.")]
+        [Phone(ErrorMessage = "The Phone Number field is not a valid phone number.")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "The Postal Code field is required.")]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "The Country field is required.")]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+    }
+}
